a39f871c7c6a3635b7bf2c74d267b65919af2445
Timur Liftiiev <timur.liftiiev@gmail.com>
2022-10-26 10:21:29 +0300

    basic project structure, tailwind config

---
+++ pages/index.jsx
@@ -0,0 +1,25 @@
+function About({ makes, models }) {
+  return <h1>Home page</h1>;
+}
+
+export async function getStaticProps() {
+  try {
+    const response = await fetch(`${process.env.API}/makes`);
+    const makesData = await response.json();
+
+    const makes = makesData.map((makeObj) => makeObj.brand);
+
+    const models = makesData.reduce((acc, make) => {
+      return {
+        ...acc,
+        [make.brand]: make.models,
+      };
+    }, {});
+
+    return { props: { makes, models } };
+  } catch (e) {
+    return { notFound: true };
+  }
+}
+
+export default About;
